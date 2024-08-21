93f99b915942bf267590029fe3d80eef1d9599f3
anthonyc-a <anthonyc.animba@gmail.com>
2024-08-21 17:10:11 +0700

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -112,7 +112,7 @@
     {/if}
     {#if savedName === ""}
       <h1
-        class="text-3xl text-center md:text-3xl max-w-2xl tracking-  font-[525] mx-auto pb-0 w-fit"
+        class="text-3xl text-center md:text-3xl max-w-2xl tracking-  font-[500] mx-auto pb-0 w-fit"
       >
         Solving problems and creating digital experiences with clear and
         functional design.
