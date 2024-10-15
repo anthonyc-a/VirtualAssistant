464174fb045b7e8f984c7f5c53ccbf1a8026543e
Anthony <anthonyc.animba@gmail.com>
2024-10-15 18:05:10 +0700

    .

--- src/routes/3d/+page.svelte
+++ src/routes/3d/+page.svelte
@@ -5,16 +5,6 @@
 
 <div class="w-full rounded-2xl overflow-hidden max-w-2xl mx-auto">
   <StlViewer
-    stlPath="/choc.stl"
-    texturePaths={{
-      normal: "/textures/splash/Chocolate_Splash_Free_002_norm.tga",
-      ao: "/textures/splash/Chocolate_Splash_Free_002_ao.tga",
-      roughness: "/textures/splash/Chocolate_Splash_Free_002_rough.tga",
-      metalness: "/textures/splash/Chocolate_Splash_Free_002_metal.tga",
-    }}
-    fallbackColor="#8B4513"
-  />
-  <StlViewer
     stlPath="/chair.stl"
     texturePaths={{
   normal: "/textures/splash/Chocolate_Splash_Free_002_norm.tga",
